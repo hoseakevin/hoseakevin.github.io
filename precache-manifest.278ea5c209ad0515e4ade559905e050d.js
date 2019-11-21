30362880b74b8f5e09632ac5653f472d0d9f6e95
= <=>
2019-11-21 09:33:05 +0700

    UGD PWA

---
+++ precache-manifest.278ea5c209ad0515e4ade559905e050d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37a75a6bc3a600c12d8",
+    "url": "/css/about.d443fafb.css"
+  },
+  {
+    "revision": "66e3253ce33457217f87",
+    "url": "/css/chunk-vendors.c893da24.css"
+  },
+  {
+    "revision": "88ef68be223324111abf37c4c4dc3f61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c37a75a6bc3a600c12d8",
+    "url": "/js/about.bab9a704.js"
+  },
+  {
+    "revision": "e7859cb224ab39202988",
+    "url": "/js/app.2fcb7ed1.js"
+  },
+  {
+    "revision": "66e3253ce33457217f87",
+    "url": "/js/chunk-vendors.dd618d96.js"
+  },
+  {
+    "revision": "16ed122cc8528e23750f0fe31cd09956",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
